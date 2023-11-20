8033068d7f2a44c883615a02999e70ebc0afadd9
zhang-z-heng <142060828+zhang-z-heng@users.noreply.github.com>
2023-11-20 21:53:15 +0100

    Add files via upload

---
+++ test1/test1/test01.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+using namespace std;
+
+void swap(float m, float n)
+{
+	float temp;
+	temp = m;
+	m = n;
+	n = temp;
+}
+
+/*void main()
+{
+	float a, b;
+	cin >> a >> b;
+	swap(a, b);
+	cout << a << endl << b << endl;
+}*/
+//把实参的值ab传送给函数局部工作区相应的副本中
+//函数使用的这个副本执行必要的功能
+//函数修改的是副本的值
+//实参的值不变
+void swap1(float* m, float* n)
+{
+	float t;
+	t = *m;
+	*m = *n;
+	*n = t;
+}
+//形参实参改变
+void swap2(float *m, float *n)
+{
+	float *t;
+	t = m;
+	m = n;
+	n = t;
+}//实参不变
+void main()
+{
+	float a, b, * p1, * p2;
+	cin >> a >> b;
+	p1 = &a; p2 = &b;
+	cout << p1 << endl << p2 << endl;
+	swap1(p1, p2);
+	cout << a << endl << b << endl;
+	cout << &a << endl << &b << endl;
+}
+
+void swap3(float& m, float& n)
+{
+	float temp1;
+	temp1 = m;
+	m = n;
+	n = temp1;
+}//实参改变引用变量
---
+++ test1/test1/线性表.cpp
@@ -0,0 +1,48 @@
+﻿//线性表的初始化
+//Status InitList_Sq(SqList& L)   构造一个空的顺序表L
+/*{
+	L.elem = new ElemTyoe[MaxSize];//为顺序表分配空间
+	if (!L.elem)exit(OVERFLOW);//存储分配失败
+	L.length = 0;//空表长度为0
+	return OK;
+}*/
+//线性表的取值
+/*
+int GetElem(SqList L,int i, ElemType &e){
+  if (i<1|i>L.length)
+      return ERROR;//判断i值是否合理
+  e =L.elem[i-1];//第i-1的单元存储着第i个数据
+  return OK;
+}
+
+//顺序表上的按值查找
+int LocateElem(SqList L,ElemType e)//L中查找e的数据元素，返回其序号
+{
+    for(i = 0;i<L.length;i++)
+	    if(L.elem[i] == e) return i+1;//查找成功返回序号
+	return 0;//查找失败，返回0
+}
+ASL复杂度（n+1）/2
+
+//顺序表的插入
+Status ListInsert_Sq(Sqlist&L,int i,ElemType e){
+{
+if(i<1||i>L.length+1)return ERROR;//i值不合法
+if(L.length == MaxSize)return ERROR;//当前存储空间已满
+for(j = L.length-1;j>=i-1;j--)
+   L.elem[j+1] = L.elem[j];//插入位置及之后的元素后移
+L.elem[i-1] = e;//将新元素e放入第i个位置
+L-length++;表长增1
+return OK
+}//复杂度1/n+1（n+...+1+0)= n/2
+
+//顺序表的删除
+Status ListDelete Sq(SqList&L,int i)
+{
+if ((i<1)||(i>L.Length)) return ERROR;//i值不合法
+    for(j = i;j<=L.length-1;j++)
+L.elem[j-1] = L.elem[j];//将删除的元素之后的元素前移
+L.length--;表长减一
+return OK;
+}复杂度1/n *(n-1)n/2 = n-1/2
+
